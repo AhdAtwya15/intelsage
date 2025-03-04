abfe88f7ef503a2cf3a32dcede9e30c94796e392
AhdAtwya15 <126987914+AhdAtwya15@users.noreply.github.com>
2025-03-04 04:40:11 +0200

    Initialized IntelSega Project-login page- Ui Components [Button -Input-ErrorMsg]

---
+++ src/Validation/index.ts
@@ -0,0 +1,10 @@
+import * as yup from "yup"
+export const registerSchema = yup
+
+export const loginSchema = yup
+    .object({
+    email: yup.string().required("Email is Required!").matches(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,"Email is not Valid"),
+    password: yup.string().required("Password is Required!").min(8,"Password must be at-least 6 charcters"),
+    })
+    .required()
+
